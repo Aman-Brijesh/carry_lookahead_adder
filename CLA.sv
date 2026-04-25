329c5dbc3de31a6c8eedfac6405395d6d0818c1e
Aman Brijesh <amanbrijesh06@gmail.com>
2026-04-25 16:12:36 +0530

    Implementation of the parallelism, and a better testbench

--- CLA.sv
+++ CLA.sv
@@ -1,35 +1,154 @@
 module CLA #(
-    parameter int WIDTH = 32
+    parameter int WIDTH = 4
 )(
     input  logic [WIDTH-1:0] A,
     input  logic [WIDTH-1:0] B,
     input  logic Cin,
     output logic [WIDTH-1:0] sum,
     output logic Cout,
-    output logic V
+    output logic Pg,
+    output logic Gg
 );
 
 logic [WIDTH-1:0] P, G;
 logic [WIDTH:0]   C;
 
-integer i;
+assign P = A^B;
+assign G = A&B;
 
 
-always @(*) begin
+assign C[0] = Cin;
 
-    P = A ^ B;
-    G = A & B;
+assign C[1] = G[0] |
+                (P[0]&C[0]);
 
-    C[0] = Cin;
 
-    for (i = 0; i < WIDTH; i = i + 1) begin
-        C[i+1] = G[i] | (P[i] & C[i]);
+assign C[2] = G[1] |
+            (P[1]&G[0])
+            | (P[1]&P[0]&C[0]);
+
+
+assign C[3] = G[2] | 
+            (P[2]&G[1]) |
+            (P[2]&P[1]&G[0]) |
+            (P[2]&P[1]&P[0]&C[0]);
+
+assign C[4] = G[3] |
+            (P[3]&G[2]) |
+            (P[3]&P[2]&G[1]) |
+            (P[3]&P[2]&P[1]&G[0]) |
+            (P[3]&P[2]&P[1]&P[0]&C[0]);
+
+
+assign Pg = P[3] & P[2] & P[1] & P[0];
+
+assign Gg = G[3]|
+            (P[3] & G[2])|
+            (P[3] & P[2] & G[1])|
+            (P[3] & P[2] & P[1] & G[0]);
+
+
+assign sum  = P^C[WIDTH-1:0];
+assign Cout = C[WIDTH];
+
+endmodule
+
+module CLA16(
+    input logic [15:0] A,
+    input logic [15:0] B,
+    input logic Cin,
+    output logic [15:0] Sum,
+    output logic Cout,
+    output logic Pg,
+    output logic Gg
+);
+
+logic [3:0] Pg_block,Gg_block; //4 4 Bit blocks
+logic [4:0] Cblock;
+
+assign Cblock[0] = Cin;
+
+generate
+    for(genvar i=0;i<4;i++) begin
+        CLA #(4) block(
+            .A(A[i*4 +:4]),
+            .B(B[i*4 +: 4]),
+            .Cin(Cblock[i]),
+            .sum(Sum[i*4 +: 4]),
+            .Cout(),
+            .Pg(Pg_block[i]),
+            .Gg(Gg_block[i])
+        );
     end
+endgenerate
 
-    sum  = P ^ C[WIDTH-1:0];
-    Cout = C[WIDTH];
-    V    = C[WIDTH] ^ C[WIDTH-1];
+assign Cblock[1] = Gg_block[0] |
+                    (Pg_block[0]&Cblock[0]);
 
-end
+assign Cblock[2] = Gg_block[1] |
+                    (Pg_block[1]&Gg_block[0])
+                    | (Pg_block[1]&Pg_block[0]&Cblock[0]);
+
+assign Cblock[3] = Gg_block[2] | 
+            (Pg_block[2]&Gg_block[1]) |
+            (Pg_block[2]&Pg_block[1]&Gg_block[0]) |
+            (Pg_block[2]&Pg_block[1]&Pg_block[0]&Cblock[0]);
+
+assign Cblock[4] = Gg_block[3] |
+            (Pg_block[3]&Gg_block[2]) |
+            (Pg_block[3]&Pg_block[2]&Gg_block[1]) |
+            (Pg_block[3]&Pg_block[2]&Pg_block[1]&Gg_block[0]) |
+            (Pg_block[3]&Pg_block[2]&Pg_block[1]&Pg_block[0]&Cblock[0]);
+
+assign Cout = Cblock[4];
+
+assign Pg = Pg_block[3]
+        & Pg_block[2]
+        & Pg_block[1]
+        & Pg_block[0];
+
+
+assign Gg = Gg_block[3]
+        | (Pg_block[3] & Gg_block[2])
+        | (Pg_block[3] & Pg_block[2] & Gg_block[1])
+        | (Pg_block[3] & Pg_block[2] & Pg_block[1] & Gg_block[0]);
+ 
+endmodule
+
+module CLA32(
+    input logic [31:0] A,
+    input logic [31:0] B,
+    input logic Cin,
+    output logic [31:0] Sum,
+    output logic Cout
+);
+
+logic [1:0]Pg,Gg; //2 16 bit blocks
+logic [2:0] Cblock;
+
+assign Cblock[0] = Cin;
+
+generate
+    for(genvar i=0;i<2;i++) begin
+        CLA16 block(
+            .A(A[i*16 +:16]),
+            .B(B[i*16 +:16]),
+            .Cin(Cblock[i]),
+            .Sum(Sum[i*16 +:16]),
+            .Cout(),
+            .Pg(Pg[i]),
+            .Gg(Gg[i])
+        );
+    end
+endgenerate
+
+assign Cblock[1] = Gg[0] |
+                    (Pg[0]&Cblock[0]);
+
+assign Cblock[2] = Gg[1] |
+                    (Pg[1]&Gg[0])
+                    | (Pg[1]&Pg[0]&Cblock[0]);
+
+assign Cout = Cblock[2];
 
 endmodule
--- tb_CLA.sv
+++ tb_CLA.sv
@@ -10,20 +10,20 @@ logic Cin;
 
 logic [WIDTH-1:0] sum;
 logic Cout;
-logic V;
 
-CLA #(.WIDTH(WIDTH)) dut (
+int test_count = 0;
+
+CLA32 dut (
     .A(A),
     .B(B),
     .Cin(Cin),
-    .sum(sum),
-    .Cout(Cout),
-    .V(V)
+    .Sum(sum),
+    .Cout(Cout)
 );
 
-function logic [31:0] adder_model(
-    input logic [31:0] a,
-    input logic [31:0] b,
+function logic [WIDTH:0] adder_model(
+    input logic [WIDTH-1:0] a,
+    input logic [WIDTH-1:0] b,
     input logic cin
 );
     return a+b+cin;
@@ -31,40 +31,58 @@ function logic [31:0] adder_model(
 endfunction
 
 task add_test(
-    input logic [31:0] a,
-    input logic [31:0] b,
+    input logic [WIDTH-1:0] a,
+    input logic [WIDTH-1:0] b,
     input logic cin
 );
-    logic [31:0] expected;
+
+    logic [WIDTH:0] expected;
+    logic [WIDTH:0] actual;
+
+    test_count++;
+
+
     A = a;
     B = b;
     Cin = cin;
     
     expected = adder_model(a,b,cin);
 
-    #1
+    #1;
 
-    if(sum != expected)
-        $fatal(0, "FAIL:  A=%0d B = %0d Cin = %0d Expected = %0d Computed Sum = %0d",a,b,cin,expected,sum);
+    actual = {Cout,sum};
+
+    if(actual !== expected)
+        $fatal(0, "FAIL:  A=%0d B = %0d Cin = %0d Expected = %0d Computed Sum = %0d",a,b,cin,expected,actual);
     else
         $display("PASS:  A = %0d B = %0d Sum = %0d",a,b,sum);
 
 endtask
 
 initial begin
+    add_test(0,0,0);
+
+    add_test('1,0,0);
+
+    add_test('1,1,0);
+
+    add_test('1,1,1);
+
+    add_test(0,0,1);
+
     for(int i=0;i<1000;i++) begin
-        logic [31:0] a;
-        logic [31:0] b;
+        logic [WIDTH-1:0] a;
+        logic [WIDTH-1:0] b;
         logic cin;
 
         a = $urandom;
         b = $urandom;
-        cin = $urandom;
+        cin = $urandom_range(1,0);
         
         add_test(a,b,cin);
     end
 
-    $display("ALL TESTS COMPLETED!");
+    $display("ALL TESTS COMPLETED!, total test count: %d",test_count);
     $finish;
 end
 
